f673e897b249106d39b31fd15f393273549606e2
Monique Loibner <m.loibner@hotmail.com>
2020-07-05 16:16:54 +1000

    Variables created for API JSON info, CLASSES created and rendered for UVI based on standard color model

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const city = $('#cityInput').val()
-const queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q='
-const apiKey = '&appid=d175ce97bfe64d9a627bbac014ecb740&units=metric'
-const queryUVURL = "https://api.openweathermap.org/data/2.5/onecall?"
-let citySearch = ''
+const city = $('#cityInput').val();
+const queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q=';
+const apiKey = '&appid=d175ce97bfe64d9a627bbac014ecb740&units=metric';
+const queryUVURL = "https://api.openweathermap.org/data/2.5/onecall?";
+let citySearch = '';
 
 //Check for anything inside localStorage for city history
 let cityList = [];
@@ -12,7 +12,7 @@ if (!cityList) {
     cityList = [];
 }
 //Calling prevCity function to render HTML for searched list.
-prevCity()
+prevCity();
 
 //Submit event listener. Appending search to list in previous search div then rendering city Forecast info.    
 $('#cityInput').on('submit', function(event){
@@ -35,7 +35,7 @@ $(".searchedCities").on('click', function(){
 })
 
 //loop over previous city array and append to div. 
-const prevCity = function(){
+function prevCity(){
     if (!cityList) {
     } else {
         $(".cityList").empty();
@@ -51,19 +51,21 @@ const prevCity = function(){
 }
 
 //Function to render Forecast information
-const renderCity = function (event){
+function renderCity(event){
     event.preventDefault();
 
     const location = citySearch;
     const apiURL = queryURL + location + apiKey
 
+    //API search for weather that includes Lat and Long for location 
     $.ajax({    
         url: apiURL,
         method: 'GET'
     }).then(function(response){
         result = response
-        console.log(result)
+        console.log(result);
 
+        //Creating variables for rendering with API info later on
         let cityName = result.city.name;
         let latitude = result.city.coord.lat;
         let longitude = result.city.coord.lon;
@@ -73,25 +75,56 @@ const renderCity = function (event){
         let wind = "";
         let UV = "";
 
-        const UVSearch = queryUVURL + "lat=" + latitude + "&lon=" + longitude + "&exclude=hourly,minutelyy&units=metric" + apiKey
+        //API for weather search that includes UV index
+        const uVSearch = queryUVURL + "lat=" + latitude + "&lon=" + longitude + "&exclude=hourly,minutelyy&units=metric" + apiKey;
+        console.log(uVSearch);
         $.ajax({
-            url = UVSearch,
-            method = "GET"
+            url: uVSearch,
+            method: "GET"
         }).then(function(response){
 
+            //Clear all classes for new search info to be rendered
             $(".todayName").empty();
             $(".todayTemp").empty();
             $(".todayHumidity").empty();
             $(".todayWind").empty();
             $(".todayUV").empty();
 
+            //Gather information from API JSON to store in variables
             date = moment(response.current.dt * 1000).format('L');
             weatherIcon = response.current.weather[0].icon;
-            humidity = response.current.humidity
+            humidity = response.current.humidity;
             wind = response.current.wind_speed;
-            UV = response.current.uvi
-            const title = cityName + date + weatherIcon
-            temperature = response.current.temp
+            UV = response.current.uvi;
+            const title = cityName + date;
+            temperature = response.current.temp;
+
+            let weatherIconURL = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
+
+            //Appending info to class sections in HTML to render on page
+            $(".todayName").text(title);
+            $(".todayIcon").attr("src", weatherIconURL);
+            $(".todayTemp").append("temperature: " + temperature + "&#8451;");
+            $(".todayHumidity").text("Humidity: " + humidity + "%");
+            $(".todayWind").text("Wind Speed: " + wind + "km/h");
+            $(".todayUV").append("UV index: <span id='indexUV'>" + UV + "<span>")
+
+            //Change color background of UV index
+            if (UV <= 2) {
+                $("#indexUV").addClass("low");
+            }
+            if (UV > 2 && UV < 5) {
+                $("#indexUV").addClass("moderate");
+            }
+            if (UV > 5 && UV < 7) {
+                $("#indexUV").addClass("high");
+            }
+            if (UV > 7 && UV < 10) {
+                $("#indexUV").addClass("veryHigh");
+            }
+            if (UV >= 10) {
+                $("#indexUV").addClass("extreme");
+            }
         })
     })
 }
